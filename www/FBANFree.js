b4aa1e349c9802a726419c90f9efe371f60d0768
wichanan <wicher_non@live.com>
2019-05-11 03:40:02 +0700

    Update the plugin dependencies

--- www/FBANFree.js
+++ www/FBANFree.js
@@ -1,13 +1,3 @@
 var exec = require('cordova/exec');
 
-exports.echo = function (arg0, success, error) {
-    exec(success, error, 'FBANFree', 'echo', [arg0]);
-};
 
-exports.echojs = function (arg0, success, error) {
-	if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
-		success(arg0);
-	} else {
-		error('Empty message');
-	}
-}
