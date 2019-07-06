77d389ff674f25e93dc0c81bc3ecaa4e929d4e6a
wichanan <wicher_non@live.com>
2019-07-07 01:13:00 +0700

    Finish up the android banner ad

--- src/android/ads/BannerAd.java
+++ src/android/ads/BannerAd.java
@@ -1,32 +1,31 @@
 package fban.plugin.ads;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.FrameLayout;
 
 import com.facebook.ads.Ad;
 import com.facebook.ads.AdError;
 import com.facebook.ads.AdListener;
 import com.facebook.ads.AdSize;
 import com.facebook.ads.AdView;
-import com.facebook.ads.AudienceNetworkAds;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.PluginResult;
 
 import fban.plugin.Action;
+import fban.plugin.Events;
 
 public class BannerAd extends AdBase {
     private static final String TAG = "FBAN-Free()::BannerAD";
     private AdView adView;
     private ViewGroup parentView;
-    private int gravity;
 
-    BannerAd(int id, String placementID, int gravity) {
+    BannerAd(int id, String placementID) {
         super(id, placementID);
-        this.gravity = gravity;
     }
 
     public static boolean executeShowAction(Action action, CallbackContext callbackContext) {
@@ -38,8 +37,7 @@ public class BannerAd extends AdBase {
                 if (bannerAd == null) {
                     bannerAd = new BannerAd(
                             action.optId(),
-                            action.getPlacementID(),
-                            Gravity.BOTTOM
+                            action.getPlacementID()
                     );
                 }
                 bannerAd.show();
@@ -70,6 +68,7 @@ public class BannerAd extends AdBase {
 
     public void hide() {
         if (adView != null) {
+            Log.d(TAG, "tryin to hide the banner");
             adView.setVisibility(View.GONE);
         }
     }
@@ -99,45 +98,55 @@ public class BannerAd extends AdBase {
             @Override
             public void onError(Ad ad, AdError adError) {
                 Log.d(TAG, "Error showing ad with" + adError.getErrorMessage());
+                AdBase.plugin.emit(Events.BANNER_LOAD_FAIL);
             }
 
             @Override
             public void onAdLoaded(Ad ad) {
-                Log.d(TAG, "Ad loaded");
+                AdBase.plugin.emit(Events.BANNER_LOAD);
             }
 
             @Override
             public void onAdClicked(Ad ad) {
-                Log.d(TAG, "Ad clicked" );
+                AdBase.plugin.emit(Events.BANNER_CLICK);
             }
 
             @Override
             public void onLoggingImpression(Ad ad) {
-                Log.d(TAG, "ad impression");
+                AdBase.plugin.emit(Events.BANNER_IMPRESSION);
             }
         });
     }
 
     private void addBannerView(AdView adView) {
+        float dip = 50f;
+        Resources r = plugin.webView.getView().getResources();
+        float px = pxFromDp(plugin.webView.getContext(), dip);
+
+        FrameLayout webView = (FrameLayout) plugin.webView.getView().getParent();
+        int adPosition = webView.getHeight() - (int)Math.floor(px);
+
         View view = plugin.webView.getView();
-        ViewGroup wvParentView = (ViewGroup) view.getParent();
-        if (parentView == null) {
-            parentView = new LinearLayout(plugin.webView.getContext());
-        }
 
-        if (wvParentView != null && wvParentView != parentView) {
-            wvParentView.removeView(view);
-            LinearLayout content = (LinearLayout) parentView;
-            content.setOrientation(LinearLayout.VERTICAL);
-            parentView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, 0.0F));
-            view.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, 1.0F));
-            parentView.addView(view);
-            wvParentView.addView(parentView);
-        }
+        webView.removeView(view);
+        FrameLayout.LayoutParams webViewParams = new FrameLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                adPosition
+        );
+        view.setLayoutParams(webViewParams);
 
-        parentView.addView(adView);
-        parentView.bringToFront();
-        parentView.requestLayout();
-        parentView.requestFocus();
+        webView.addView(view);
+
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        params.setMargins(0, adPosition, 0, 0);
+        adView.setLayoutParams(params);
+
+        webView.addView(adView);
+    }
+
+    private static float pxFromDp(final Context context, final float dp) {
+        return dp * context.getResources().getDisplayMetrics().density;
     }
 }
